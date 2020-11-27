1bededa7d6ea4f92540881001be8158125ca2cf6
Saeed-2040 <atraf.aljazeera@hotmail.com>
2020-11-27 22:35:11 +0400

    End of Section 3

---
+++ ZwajApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZwajApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [StringLength(8,MinimumLength=4,ErrorMessage=" يجب أن لا تزيد كلمة السر عن ثمانية أحرف ولا تقل عن أربعة أحرف")]
+        public string password { get; set; }
+    }
+}
