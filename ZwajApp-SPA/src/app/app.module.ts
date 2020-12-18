1d0c28ff72bf8d2efc95549a6693b59de08167ba
Saeed-2040 <atraf.aljazeera@hotmail.com>
2020-12-18 18:13:55 +0400

    End of Section 6

--- ZwajApp-SPA/src/app/app.module.ts
+++ ZwajApp-SPA/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvidor } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [			
@@ -23,11 +25,13 @@ import { ErrorInterceptorProvidor } from './_services/error.interceptor';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthService ,
-    ErrorInterceptorProvidor
+    ErrorInterceptorProvidor,
+    AlertifyService
   ],
   bootstrap: [AppComponent]
 })
--- ZwajApp-SPA/src/app/nav/nav.component.ts
+++ ZwajApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ok } from 'assert';
+import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
 @Component({
@@ -9,25 +10,24 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any={};
-  constructor(private authServise : AuthService) { }
+  constructor(public authService : AuthService,private alertify:AlertifyService) { }
 
   ngOnInit() {
   }
 
   login(){
-    this.authServise.login(this.model).subscribe(
-      next=>{console.log('تم الدخول بنجاح')},
-      error=>{console.log(error)}
+    this.authService.login(this.model).subscribe(
+      next=>{this.alertify.success('تم الدخول بنجاح')},
+      error=>{this.alertify.error(error)}
       
     )
   }
   loggedIn(){
-    const token=localStorage.getItem('token');
-    return !! token
+   return this.authService.loggedIn();
   }
   loggedOut(){
     localStorage.removeItem('token');
-    console.log('تم الخروج')
+    this.alertify.message('تم الخروج')
   }
 
 }
