3e9532a95342f4a7a32672d2e990c452d68e913f
Saeed-2040 <atraf.aljazeera@hotmail.com>
2020-12-23 23:15:27 +0400

    End of Section 7

--- ZwajApp-SPA/src/app/nav/nav.component.ts
+++ ZwajApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ok } from 'assert';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
@@ -10,7 +11,7 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any={};
-  constructor(public authService : AuthService,private alertify:AlertifyService) { }
+  constructor(public authService : AuthService,private alertify:AlertifyService,private router:Router) { }
 
   ngOnInit() {
   }
@@ -18,7 +19,8 @@ export class NavComponent implements OnInit {
   login(){
     this.authService.login(this.model).subscribe(
       next=>{this.alertify.success('تم الدخول بنجاح')},
-      error=>{this.alertify.error(error)}
+      error=>{this.alertify.error(error)},
+      ()=>{this.router.navigate(['/members']);}
       
     )
   }
@@ -27,7 +29,8 @@ export class NavComponent implements OnInit {
   }
   loggedOut(){
     localStorage.removeItem('token');
-    this.alertify.message('تم الخروج')
+    this.alertify.message('تم الخروج');
+    this.router.navigate(['/home']);
   }
 
 }
