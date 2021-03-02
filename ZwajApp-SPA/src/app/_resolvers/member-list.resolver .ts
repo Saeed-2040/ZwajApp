ef0d5e9bd11b3914a79531215fbfc262b6a0eedc
Saeed-2040 <atraf.aljazeera@hotmail.com>
2021-03-02 22:19:34 +0400

    End Of Section 9

---
+++ ZwajApp-SPA/src/app/_resolvers/member-list.resolver .ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, Router } from "@angular/router";
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { User } from "../_models/user";
+import { AlertifyService } from "../_services/alertify.service";
+import { UserService } from "../_services/user.service";
+
+@Injectable()
+export class MemberlistResolver implements Resolve<User[]>{
+    constructor(private userService:UserService, private router:Router,private alertify:AlertifyService){}
+    resolve(route:ActivatedRouteSnapshot):Observable<User[]>{
+      return this.userService.getUsers().pipe(
+          catchError(error=>{
+              this.alertify.error('يوجد مشكلة في عرض البيانات');
+              this.router.navigate(['']);
+              return of(null)
+          })
+      )
+    }
+}
